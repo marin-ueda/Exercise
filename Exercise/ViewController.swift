6b3abc6b72c330c68625686f77cceadf0d5e3754
Marin Ueda <marin96nym@gmail.com>
2020-05-20 10:38:09 +0900

    Initial Commit

---
+++ Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercise
+//
+//  Created by 植田真梨 on 2020/05/20.
+//  Copyright © 2020 Ueda Maririn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
