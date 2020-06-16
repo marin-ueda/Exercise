fdbabf427f35eecc31ca28744c87190717e453f6
Marin Ueda <marin96nym@gmail.com>
2020-06-16 23:47:49 +0900

    ポイント数に応じて、背景色・画像数を増やす。 serihuLabelとimageViewが消せない。

--- Exercise/ExerciseViewController.swift
+++ Exercise/ExerciseViewController.swift
@@ -12,16 +12,22 @@ import AVFoundation
 
 class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
     
+
+    
     //0614 メッセージ入れる
-    @IBOutlet var img: UIImageView!
+    @IBOutlet var image: UIImageView!
     @IBOutlet var hukidasi: UIImageView!
     @IBOutlet var serihuLabel: UILabel!
     
     //0613 音楽入れる
     var audioPlayer:AVAudioPlayer!
+//    var audioPlayer2:AVAudioPlayer!
     
     //0608Realm入れてみる
     let realm = try! Realm()
+    
+    let kiroku = try! Realm().objects(Kiroku.self).sorted(byKeyPath: "hiniti")
+    var notificationToken: NotificationToken?
     
     //0610new 日付をRealmに
     let dt = Date()
@@ -45,35 +51,17 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
     var count: Double = 3
     var timer: Timer = Timer()
     
-    //0609日付情報取得
-    // 現在日時を取得
-    /*    let dt = Date()
-     let formatter: DateFormatter = DateFormatter()*/
-    
-    //0610 日付リベンジ！
-    /*    lazy var dateFormatter: DateFormatter = {
-     var formatter = DateFormatter()
-     formatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "ydMMM",
-     options: 0,
-     locale: Locale(identifier: "ja_JP"))
-     formatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
-     return formatter
-     }()*/
-    
-    
+
     
     //0608Realmを入れてみる(変えるのはここか？？日付入れる、合計秒数にする）
     func addKiroku() {
         let newkiroku = Kiroku()
         newkiroku.seconds = narg!
-        //0610new 日付をRealmに
-        // DateFormatter を使用して書式とロケールを指定する
-//        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMMMd", options: 0, locale: Locale(identifier: "en_US_POSIX"))
+
         dateFormatter.dateStyle = .medium
         dateFormatter.timeStyle = .none
         dateFormatter.locale = Locale(identifier: "ja_JP")
-//        dateFormatter.doesRelativeDateFormatting = true
-        //        print(dateFormatter.string(from: dt))
+
         newkiroku.hiniti = dateFormatter.string(from: dt)
         
         //0612もし日付が同じだったら、秒数のところだけを更新
@@ -88,7 +76,7 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
             }
         } else {
             try! realm.write {
-                realm.add(newkiroku/*, update: .all*/)
+                realm.add(newkiroku)
             }
         }
     }
@@ -99,10 +87,19 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        UserDefaults.standard.set(now_day, forKey: "date")
+        //0608Realm入れてみる
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        //0613　改：Realmの更新があったらすぐに反映
+        notificationToken = kiroku.observe { [weak self] _ in
+            self?.Point()
+        }
         //0613 音入れる
         // 再生する audio ファイルのパスを取得
         let audioPath = Bundle.main.path(forResource: "sound36", ofType:"mp3")!
         let audioUrl = URL(fileURLWithPath: audioPath)
+//        let audioPath2 = Bundle.main.path(forResource: "sound09", ofType:"mp3")!
+//        let audioUrl2 = URL(fileURLWithPath: audioPath2)
         // auido を再生するプレイヤーを作成する
         var audioError:NSError?
         do {
@@ -112,13 +109,27 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
             audioPlayer = nil
         }
         
+/*        var audioError2:NSError?
+        do {
+            audioPlayer2 = try AVAudioPlayer(contentsOf: audioUrl2)
+        } catch let error as NSError {
+            audioError2 = error
+            audioPlayer2 = nil
+        }*/
+        
         // エラーが起きたとき
         if let error = audioError {
             print("Error \(error.localizedDescription)")
         }
+        // エラーが起きたとき2
+/*        if let error2 = audioError2 {
+            print("Error \(error2.localizedDescription)")
+        }*/
         
         audioPlayer.delegate = self
         audioPlayer.prepareToPlay()
+//        audioPlayer2.delegate = self
+//        audioPlayer2.prepareToPlay()
         
         //0601前ページからタイマーの数字持ってくる
         //引数をラベルにセット
@@ -126,8 +137,7 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
         //0606　argを小数型に変換
         narg = Double(arg)
         
-        //0608Realm入れてみる
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
+
         
         
         //メニュー名を表示(Quizアプリ参照)
@@ -151,11 +161,11 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
         //0602自動的にスタート
         start()
         start2()
+        Point()
         
-        messege()
-        
-        
+//        messege()
     }
+    
     func menua() {
         if tappedBtnTag == 1 {
             menuLabel.text = String("腹筋")
@@ -171,19 +181,58 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
             menuLabel.text = String("ストレッチ")
         }
     }
-    
-    //0614 メッセージ入れる
-    func messege() {
-        let serihu:[String] = ["その調子！その調子！", "できてるよ！頑張れ〜", "ファイトー！", "筋肉意識してね。", "呼吸大事だよ！", "息吸ってーはいてー", "肩の力抜いて！", "運動している君は偉い！", "毎日頑張ろう！", "明日もっと美しく！", "ナイスファイト", "もっといける！", "続ける事が大事！", "コツコツ頑張ろう！", "憧れに近づいてるよ！"]
-        let nu = arc4random_uniform(UInt32(serihu.count))
+    //0616
+    func Point() {
+        let point = realm.objects(Kiroku.self)
+        let summ: Int = point.sum(ofProperty: "seconds")
+        let img1 = UIImage(named:"zou.png")
+        let img2 = UIImage(named: "cat.png")
+        let img3 = UIImage(named: "buta.png")
+        let img4 = UIImage(named: "cat_green.png")
+        let img5 = UIImage(named: "penpen.png")
+        let img6 = UIImage(named: "tako.png")
+        let img7 = UIImage(named: "kuzira.png")
 
-        serihuLabel.text = serihu[Int(nu)]
+        let serihu:[String] = ["その調子！その調子！", "頑張れ〜!", "ファイトー！", "筋肉意識してね。", "呼吸大事だよ！", "息吸ってーはいてー", "肩の力抜いて！", "運動している君は偉い！", "毎日頑張ろう！", "明日もっと美しく！", "ナイスファイト", "もっといける！", "続ける事が大事！", "コツコツ頑張ろう！", "憧れに近づいてるよ！", "やればできる！", "目指せ10000ポイント！"]
+
+
+        if summ >= 180 && summ <= 30000 {
+            let ii:[UIImage?] = [img1, img2, img3, img4, img5, img6, img7]
+            view.backgroundColor = UIColor.init(red: 255/255, green: 249/255, blue: 128/255, alpha: 1)
+            let nu = arc4random_uniform(UInt32(ii.count))
+            image.image = ii[Int(nu)]
+
+        } else if summ >= 150 {
+            let iii:[UIImage?] = [img4, img6, img3]
+            view.backgroundColor = UIColor.init(red: 227/255, green: 219/255, blue: 255/255, alpha: 1)
+            let nu = arc4random_uniform(UInt32(iii.count))
+            image.image = iii[Int(nu)]
+        } else {
+            let iiii:[UIImage?] = [img1]
+            view.backgroundColor = UIColor.init(red: 194/255, green: 247/255, blue: 159/255, alpha: 1)
+            let nu = arc4random_uniform(UInt32(iiii.count))
+            image.image = iiii[Int(nu)]
+        }
+
+        let num = arc4random_uniform(UInt32(serihu.count))
+        serihuLabel.text = serihu[Int(num)]
+
     }
+
+    
     func nonmessege() {
         serihuLabel.text = ""
         hukidasi.image = nil
-        img.image = nil
+        image.image = nil
     }
+    
+        //0614 メッセージ入れる
+    /*    func messege() {
+            let serihu:[String] = ["その調子！その調子！", "頑張れ〜!", "ファイトー！", "筋肉意識してね。", "呼吸大事だよ！", "息吸ってーはいてー", "肩の力抜いて！", "運動している君は偉い！", "毎日頑張ろう！", "明日もっと美しく！", "ナイスファイト", "もっといける！", "続ける事が大事！", "コツコツ頑張ろう！", "憧れに近づいてるよ！", "やればできる！"]
+            let nu = arc4random_uniform(UInt32(serihu.count))
+
+            serihuLabel.text = serihu[Int(nu)]
+        }*/
     
     
     
@@ -196,25 +245,24 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
         count = count - 0.1
         //ラベルに小数点以下1桁まで表示
         TimerLabel.text = String(format: "%.0f", count)
-        if (count < 0.5) {
+        if count < 0.5 {
+//            audioPlayer2.play()
             //            TimerLabel.text = String("START")
             //タイマーが動作していたら停止する
             //            timer.invalidate()
             self.down2()
-            
         }
     }
     @objc func down2() {
-        //countを0.1を引く
+     //countを0.1を引く
         narg = narg - 0.1
         //ラベルに小数点以下1桁まで表示
         TimerLabel.text = String(format: "%.1f", narg)
         self.textColor()
-        
         if (narg <= 0) {
             //タイマーが動作していたら停止する
             timer.invalidate()
-            TimerLabel.font = TimerLabel.font.withSize(50)
+            TimerLabel.font = TimerLabel.font.withSize(52)
             let argba = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0)
             TimerLabel.textColor = argba
             TimerLabel.text = String("Nice Fight!!")
@@ -223,6 +271,8 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
             addKiroku()
             audioPlayer.play()
             nonmessege()
+
+//            saveData.set(now_day, forKey: "date")
         }
     }
     
@@ -249,6 +299,11 @@ class ExerciseViewController: UIViewController, AVAudioPlayerDelegate {
     //完了ボタンでトップページに戻る
     @IBAction func modoru() {
         self.dismiss(animated: true, completion: nil)
+        if timer.isValid {
+            timer.invalidate()
+            count = 3
+            narg = Double(arg)
+        }
     }
     
     //0603タイマーの色を秒数によって変える
--- Exercise/ViewController.swift
+++ Exercise/ViewController.swift
@@ -9,24 +9,31 @@
 import UIKit
 import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     //0601前ページからタイマーの数字持ってくる
     @IBOutlet weak var textField1: UITextField!
-/*    //Datepiker入れてみた
-    @IBOutlet var datePiker: UIDatePicker!*/
+    /*    //Datepiker入れてみた
+     @IBOutlet var datePiker: UIDatePicker!*/
     
     @IBOutlet var pointLabel: UILabel!
     @IBOutlet var nissuLabel: UILabel!
     @IBOutlet var pointhaikei: UILabel!
+    @IBOutlet var p: UIImageView!
+    
+//    let saveData: UserDefaults = UserDefaults.standard
+    
     
     //0613 再：日付をRealmに
     let dt = Date()
     let dateFormatter = DateFormatter()
     
     //合計日数のための関数
-    var hi: Int = 0
-  
+//    var hi: Int = 0
+    //日本時間を設定
+//    let now_day = Date()
+
+    
     //0608Realm入れてみる
     let realm = try! Realm()
     let kiroku = try! Realm().objects(Kiroku.self).sorted(byKeyPath: "hiniti")
@@ -50,105 +57,206 @@ class ViewController: UIViewController {
         print(Realm.Configuration.defaultConfiguration.fileURL!)
         //0613　改：Realmの更新があったらすぐに反映
         notificationToken = kiroku.observe { [weak self] _ in
-        self?.Points()
-            
+            self?.Points()
+            self?.goukei()
+            //        self?.nissu()
+        }
+        
+        //0615 UserDefaultsの更新があったらすぐに反映
+/*        UserDefaults.standard.set(hi, forKey: "kaisu")
+        UserDefaults.standard.set(now_day, forKey: "date")*/
+        
+
         //0613 ボタンの角を丸くする
-        self?.hukkin.layer.cornerRadius = 12.0
-        self?.neziri.layer.cornerRadius = 12.0
-        self?.sukuwatto.layer.cornerRadius = 12.0
-        self?.taikan.layer.cornerRadius = 12.0
-        self?.sonota.layer.cornerRadius = 12.0
-        self?.haikin.layer.cornerRadius = 12.0
-        self?.nissuLabel.layer.cornerRadius = 10.0
-        self?.pointhaikei.layer.cornerRadius = 10.0
-            //外枠の色を指定
-/*            self?.hukkin.layer.borderColor = UIColor.cyan.cgColor
-            self?.neziri.layer.borderColor = UIColor.cyan.cgColor
-            self?.sukuwatto.layer.borderColor = UIColor.cyan.cgColor
-            self?.taikan.layer.borderColor = UIColor.cyan.cgColor
-            self?.haikin.layer.borderColor = UIColor.cyan.cgColor
-            self?.sonota.layer.borderColor = UIColor.cyan.cgColor
-             
-            //外枠の太さを指定
-            self?.hukkin.layer.borderWidth = 7.0
-            self?.neziri.layer.borderWidth = 7.0
-            self?.sukuwatto.layer.borderWidth = 7.0
-            self?.taikan.layer.borderWidth = 7.0
-            self?.haikin.layer.borderWidth = 7.0
-            self?.sonota.layer.borderWidth = 7.0*/
+        hukkin.layer.cornerRadius = 12.0
+        neziri.layer.cornerRadius = 12.0
+        sukuwatto.layer.cornerRadius = 12.0
+        taikan.layer.cornerRadius = 12.0
+        sonota.layer.cornerRadius = 12.0
+        haikin.layer.cornerRadius = 12.0
+        nissuLabel.layer.cornerRadius = 10.0
+        pointhaikei.layer.cornerRadius = 10.0
+        //外枠の色を指定
+        /*            self?.hukkin.layer.borderColor = UIColor.cyan.cgColor
+         self?.neziri.layer.borderColor = UIColor.cyan.cgColor
+         self?.sukuwatto.layer.borderColor = UIColor.cyan.cgColor
+         self?.taikan.layer.borderColor = UIColor.cyan.cgColor
+         self?.haikin.layer.borderColor = UIColor.cyan.cgColor
+         self?.sonota.layer.borderColor = UIColor.cyan.cgColor
+         
+         //外枠の太さを指定
+         self?.hukkin.layer.borderWidth = 7.0
+         self?.neziri.layer.borderWidth = 7.0
+         self?.sukuwatto.layer.borderWidth = 7.0
+         self?.taikan.layer.borderWidth = 7.0
+         self?.haikin.layer.borderWidth = 7.0
+         self?.sonota.layer.borderWidth = 7.0*/
+        
+        
+        
+        //0613キーボードを閉じる
+        textField1.delegate = self
+        //0615 UserDefaultsの型をDate型にする
+        func load(key: String) -> Date? {
+            let saveData = UserDefaults.standard.object(forKey: "date")
+            guard let date = saveData as? Date else {
+                return nil
+            }
+            return date
         }
         
         //0613 ここ入れる！
         
         
         Points()
+//        judgeDate()
         goukei()
-//        nissu()
+        //        nissu()
     }
     
-
+    
     //0613 キーボードをreturnで閉じる
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // キーボードを閉じる
         textField.resignFirstResponder()
         return true
+    }
+    //0614 他をタッチでキーボード閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     //0613 point入れる！
     func Points() {
         let point = realm.objects(Kiroku.self)
         let summ: Int = point.sum(ofProperty: "seconds")
-        pointLabel.text = String(summ)
-    }
-    //0613 合計日数
-    func goukei() {
-           let gou = realm.objects(Kiroku.self)
-           let sum: Int = gou.count
-
-            nissuLabel.text = String("Day \(sum)")
-
-       }
-    
-/*    //0613 連続する日数を出す!!
-    func nissu() {
-//   let f = DateFormatter()
-//    f.dateStyle = .medium
-//    f.locale = Locale(identifier: "ja_JP")
-    let now = Date()
-//    print(f.string(from: now))
-    
-    var calendar = Calendar.current
-    let day = calendar.component(.day, from: now)
-    
-    let components = calendar.dateComponents([.day],
-                                                 from: fromDate,
-                                                 to: now)
-    let passTime = components.day!
-        if passTime <= 1 {
-            hi += 1
-        } else {
-            hi = 0
-        }
-        nissuLabel.text = String(hi)
+        let img1 = UIImage(named:"point3yellow.png")
+        let img2 = UIImage(named: "point_orange.png")
+        let img3 = UIImage(named: "point_blue.png")
         
+        if summ > 20000 && summ <= 30000 {
+            p.image = img3
+            pointLabel.text = String(summ - 20000)
+            view.backgroundColor = UIColor.init(red: 255/255, green: 249/255, blue: 128/255, alpha: 1)
+        } else if summ > 10000 {
+            p.image = img2
+            pointLabel.text = String(summ - 10000)
+            view.backgroundColor = UIColor.init(red: 227/255, green: 219/255, blue: 255/255, alpha: 1)
+        } else {
+            p.image = img1
+            pointLabel.text = String(summ)
+            view.backgroundColor = UIColor.init(red: 194/255, green: 247/255, blue: 159/255, alpha: 1)
+        }
+
+    }
+        //0613 合計日数
+     func goukei() {
+     let gou = realm.objects(Kiroku.self)
+     let sum: Int = gou.count
+     
+     nissuLabel.text = String("Day \(sum)")
+     
+     }
+    
+    //0613 連続する日数を出す!!
+    /*    func nissu() {
+     //   let f = DateFormatter()
+     //    f.dateStyle = .medium
+     //    f.locale = Locale(identifier: "ja_JP")
+     let now = Date()
+     //    print(f.string(from: now))
+     
+     var calendar = Calendar.current
+     let day = calendar.component(.day, from: now)
+     saveData.set(now, forKey: "date")
+     let fromDate = saveData.object(forKey: "date")
+     
+     let components = calendar.dateComponents([.day],
+     from: Date(fromDate),
+     to: now)
+     
+     let passTime = components.day!
+     if passTime <= 1 {
+     hi += 1
+     } else {
+     hi = 0
+     }
+     nissuLabel.text = String(hi)
+     
+     }*/
+    //日付判定関数
+/*    func judgeDate(){
+        //現在のカレンダ情報を設定
+        _ = Calendar.current
+        //日付判定結果
+//        var judge = Bool()
+        // 日時経過チェック
+        if saveData.object(forKey: "date") != nil {
+            let past_day = saveData.object(forKey: "date") as! Date
+//            let past_day = Date(timeIntervalSinceReferenceDate: past_d)
+//            let now = calender.component(.day, from: now_day)
+//            let past = calender.component(.day, from: past_day)
+            let dayInterval = (Calendar.current.dateComponents([.day], from: past_day, to: now_day)).day
+            print(dayInterval!)
+            
+            //日にちが変わっていた場合
+/*            if now != past {
+                judge = true
+            }
+            else {
+                judge = false
+            }
+            /* 日付が変わった場合はtrueの処理 */
+            if judge == true {
+                judge = false*/
+                if dayInterval! == 1 {
+                    hi += 1
+                    saveData.set(hi, forKey: "kaisu")
+                nissuLabel.text = String("連続 \(saveData.object(forKey: "kaisu")!)日")
+                } else if dayInterval! >= 2 {
+                    hi = 0
+                    saveData.set(hi, forKey: "kaisu")
+                    nissuLabel.text = String("毎日やろう！")
+                } else if dayInterval! == 0 {
+                    nissuLabel.text = String("すごいぞ！！ \(saveData.object(forKey: "kaisu")!)日")
+            }
+
+        } else {
+            nissuLabel.text = String("連続日数")
+            
+        }
+//            }
+/*            else {
+                
+            }
+        
+        }
+            //初回実行のみelse
+        else {
+            judge = true*/
+            /* 今の日時を保存 */
+
+
+        
+
     }*/
     
     @IBAction func KirokuButtonTapped() {
         _ = UserDefaults.standard
-/*        //0610 日付
-          let dateFormater = DateFormatter()
-             dateFormater.locale = Locale(identifier: "ja_JP")
-             dateFormater.dateFormat = "yyyy/MM/dd HH:mm:ss"
-             let date = dateFormater.string(from: Date())
-             print(date)     // 2017/04/04 10:44:31*/
+        /*        //0610 日付
+         let dateFormater = DateFormatter()
+         dateFormater.locale = Locale(identifier: "ja_JP")
+         dateFormater.dateFormat = "yyyy/MM/dd HH:mm:ss"
+         let date = dateFormater.string(from: Date())
+         print(date)     // 2017/04/04 10:44:31*/
         // Realmのオブジェクトを取得
         let kosuu = realm.objects(Kiroku.self)
         //0613 Realmに入っているデータの個数
-//        print("kosuu.count: \(kosuu.count)")
+        //        print("kosuu.count: \(kosuu.count)")
         
         if kosuu.count != 0 {
             performSegue(withIdentifier: "toKirokuTableViewController", sender: nil)
@@ -183,115 +291,115 @@ class ViewController: UIViewController {
         
         self.present(alert, animated: true, completion: nil)
     }
-
+    
     //0531sender.tagで変数を作る
     // ボタンタップしてタグを取得、遷移する
     @IBAction func hukkin(_ sender: UIButton) {
         if textField1.text == nil || textField1.text == ""  {
             alert()
         } else {
-        let tagNumber = sender.tag
-        // タグをsenderでprepareに送る
-        self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
+            let tagNumber = sender.tag
+            // タグをsenderでprepareに送る
+            self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
         }
     }
     @IBAction func neziri(_ sender: UIButton) {
         if textField1.text == nil || textField1.text == ""  {
             alert()
         } else {
-        let tagNumber = sender.tag
-        // タグをsenderでprepareに送る
-        self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
+            let tagNumber = sender.tag
+            // タグをsenderでprepareに送る
+            self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
         }
     }
     @IBAction func taikan(_ sender: UIButton) {
-     if textField1.text == nil || textField1.text == ""  {
+        if textField1.text == nil || textField1.text == ""  {
             alert()
         } else {
-        let tagNumber = sender.tag
-        // タグをsenderでprepareに送る
-        self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
+            let tagNumber = sender.tag
+            // タグをsenderでprepareに送る
+            self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
         }
     }
     @IBAction func sukuwatto(_ sender: UIButton) {
         if textField1.text == nil || textField1.text == ""  {
             alert()
         } else {
-        let tagNumber = sender.tag
-        // タグをsenderでprepareに送る
-        self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
+            let tagNumber = sender.tag
+            // タグをsenderでprepareに送る
+            self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
         }
     }
     @IBAction func haikin(_ sender: UIButton) {
         if textField1.text == nil || textField1.text == ""  {
             alert()
         } else {
-        let tagNumber = sender.tag
-        // タグをsenderでprepareに送る
-        self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
+            let tagNumber = sender.tag
+            // タグをsenderでprepareに送る
+            self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
         }
     }
     @IBAction func sonota(_ sender: UIButton) {
         if textField1.text == nil || textField1.text == ""  {
             alert()
         } else {
-        let tagNumber = sender.tag
-        // タグをsenderでprepareに送る
-        self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
+            let tagNumber = sender.tag
+            // タグをsenderでprepareに送る
+            self.performSegue(withIdentifier: "toExercise", sender: tagNumber)
         }
     }
-
-      override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
-          if segue.identifier == "toExercise" {
-              // 遷移先VCを取得して
-              if let ExcerciseVC = segue.destination as? ExerciseViewController {
-                  //遷移先VCのプロパティにアクセス、senderで送られてきたタグをIntに変換して渡す
-                  ExcerciseVC.tappedBtnTag = sender as? Int
-                
-            
-            //0601前ページからタイマーの数字持ってくる
-            //遷移先のViewControllerの取得
-            let nextView = segue.destination as! ExerciseViewController
-            //値の設定
-            nextView.arg = textField1.text!
-              }
-            
-          }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
-      }
-  
-
+        if segue.identifier == "toExercise" {
+            // 遷移先VCを取得して
+            if let ExcerciseVC = segue.destination as? ExerciseViewController {
+                //遷移先VCのプロパティにアクセス、senderで送られてきたタグをIntに変換して渡す
+                ExcerciseVC.tappedBtnTag = sender as? Int
+                
+                
+                //0601前ページからタイマーの数字持ってくる
+                //遷移先のViewControllerの取得
+                let nextView = segue.destination as! ExerciseViewController
+                //値の設定
+                nextView.arg = textField1.text!
+            }
+            
+        }
+        
+    }
+    
+    
     //腹筋ボタンを押したら、次の画面のラベルに腹筋！
-/*    @IBAction func hukkin(_ sender: Any) {
-        self.performSegue(withIdentifier: "toExercise", sender: nil)
-    }
-    @IBAction func neziri(_ sender: Any) {
-        self.performSegue(withIdentifier: "toExercise", sender: nil)
-    }
-    @IBAction func takikan(_ sender: Any) {
-        self.performSegue(withIdentifier: "toExercise", sender: nil)
-    }
-    @IBAction func sukuwatto(_ sender: Any) {
-        self.performSegue(withIdentifier: "toExercise", sender: nil)
-    }
-    @IBAction func haikin(_ sender: Any) {
-        self.performSegue(withIdentifier: "toExercise", sender: nil)
-    }
-    @IBAction func sonota(_ sender: Any) {
-        self.performSegue(withIdentifier: "toExercise", sender: nil)
-    }*/
-
-   
-
-    }
+    /*    @IBAction func hukkin(_ sender: Any) {
+     self.performSegue(withIdentifier: "toExercise", sender: nil)
+     }
+     @IBAction func neziri(_ sender: Any) {
+     self.performSegue(withIdentifier: "toExercise", sender: nil)
+     }
+     @IBAction func takikan(_ sender: Any) {
+     self.performSegue(withIdentifier: "toExercise", sender: nil)
+     }
+     @IBAction func sukuwatto(_ sender: Any) {
+     self.performSegue(withIdentifier: "toExercise", sender: nil)
+     }
+     @IBAction func haikin(_ sender: Any) {
+     self.performSegue(withIdentifier: "toExercise", sender: nil)
+     }
+     @IBAction func sonota(_ sender: Any) {
+     self.performSegue(withIdentifier: "toExercise", sender: nil)
+     }*/
+    
+    
+    
+}
 /*//Tagに番号をつけたことを宣言
-   enum actionTag: Int {
-       case action1 = 1
-       case action2 = 2
-       case action3 = 3
-       case action4 = 4
-       case action5 = 5
-       case action6 = 6
-   }*/
+ enum actionTag: Int {
+ case action1 = 1
+ case action2 = 2
+ case action3 = 3
+ case action4 = 4
+ case action5 = 5
+ case action6 = 6
+ }*/
 
